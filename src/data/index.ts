b039d620d41cec8dfd368aa8601023603e812ece
hufukuan <hufukuan@qq.com>
2026-02-03 18:08:40 +0800

    del

--- src/data/index.ts
+++
@@ -1,8 +0,0 @@
-// 统一导出所有数据
-export { profile } from './profile';
-export { educations } from './education';
-export { researches } from './research';
-export { publications } from './publications';
-export { awards } from './awards';
-export { services } from './service';
-export { skills } from './skills';
--- src/data/publications.ts
+++
@@ -1,8 +0,0 @@
-// 发表论文配置
-export const publications = [
-  {
-    authors: 'Hu F & Xing S.',
-    title: 'Abandoned Farmland Monitoring Platform V1.0',
-    info: '2023SR0382179, Authorized.',
-  },
-];
